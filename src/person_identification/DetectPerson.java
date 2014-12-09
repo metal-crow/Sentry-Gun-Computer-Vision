7faf90cfea231da0c07272afeed395f00824ca11
metal-crow <frankcrisanti@aol.com>
2014-12-08 21:40:04 -0500

    close to done, added skin detection

--- src/person_identification/DetectPerson.java
+++ src/person_identification/DetectPerson.java
@@ -15,7 +15,7 @@ public class DetectPerson {
     private static final String ProfileCascadeClassifierFile="E:/Code Workspace/.external libraries/opencv 2.4.9/sources/data/lbpcascades/lbpcascade_profileface.xml";
     
     //combined HSB values for various skin colors
-    private static final Pair<Scalar,Scalar> skinColors=Pair.with(new Scalar(9, 30, 20), new Scalar(28, 70, 90));
+    private static final Pair<Scalar,Scalar> skinColors=Pair.with(new Scalar(4, 76, 51), new Scalar(14, 179, 230));
 
     public static int isBlobHuman(Mat img){
         int blobArea=Core.countNonZero(img);//TODO figure out priority
@@ -36,16 +36,26 @@ public class DetectPerson {
         }
         
         //next check for skin, see if the blob contains skin color
-        //find HSV values for all skin colors to check for
+        //this can be falsely positive (peach walls) or falsely negative (weird lighting)
+        Mat skin=new Mat();
+        Core.inRange(img, skinColors.getValue0(), skinColors.getValue1(), skin);
+        //if there is a non-insignificant amount of skin
+        int amountOfSkin=Core.countNonZero(skin);
+        if(amountOfSkin>img.cols()*3){
+            return amountOfSkin;
+        }
+        
+        
+
         return 0;
     }
     
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         
-        Mat image = Highgui.imread("testing/skin colors.png");
+        Mat image = Highgui.imread("testing/face3.jpg");
         Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2HSV);
-        
+                
         Core.inRange(image, skinColors.getValue0(), skinColors.getValue1(), image);
                 
         //Save the visualized detection.
