28cb753964bedae231876c8936f134348f65583f
metal-crow <frankcrisanti@aol.com>
2015-04-04 14:06:43 -0400

    temp disables

--- src/processing/DetectPerson.java
+++ src/processing/DetectPerson.java
@@ -37,7 +37,9 @@ public class DetectPerson {
         //first check if we can find a face in this blob. If so, we have a guaranteed person.
         //the priority must be > than 2 things, so add the maximum 2 times 
         MatOfRect faceDetections = new MatOfRect();
+        
         //check for a frontally facing face
+        //TODO this is generating mulitple false positives, currently disabled
         CascadeClassifier frontalFaceDetector = new CascadeClassifier(FrontalCascadeClassifierFile);
         //frontalFaceDetector.detectMultiScale(img, faceDetections);XXX TEMPORARY ONLY
         if(faceDetections.toArray().length>0){
@@ -55,7 +57,8 @@ public class DetectPerson {
         
         //next check for skin, see if the blob contains skin color
         //this can be falsely positive (peach walls) or falsely negative (weird lighting)
-        Mat skin=new Mat();
+        //TODO temp disabled b/c false positives, need to alter skin hsv range
+        /*Mat skin=new Mat();
         Core.inRange(img, skinColors.getValue0(), skinColors.getValue1(), skin);
         //if there is a non-insignificant amount of skin
         int amountOfSkin=Core.countNonZero(skin);
@@ -63,7 +66,7 @@ public class DetectPerson {
             System.out.println("Detected skin");//DEBUGGING
             //the priority must be > than 1 thing, so add the maximum 1 time
             return amountOfSkin+frameArea;
-        }
+        }*/
         
         //if we find neither, the size of the blob is the priority
         return blobArea;
